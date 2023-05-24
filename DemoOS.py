c6791cf135cddb9e487b37834a3e5f57249ae6be
papasmf1 <papasmf1@gmail.com>
2023-05-24 14:47:32 +0900

    내부 라이브러리 연습

---
+++ DemoOS.py
@@ -0,0 +1,22 @@
+# DemoOS.py 
+from os.path import * 
+
+print( abspath("python.exe") )
+print( basename("c:\\work\\demo.txt") )
+
+if exists("c:\\python310\\python.exe"):
+    print("파일크기:{0}".format(getsize("c:\\python310\\python.exe")))
+else:
+    print("파일이 없습니다.")
+
+import os 
+
+print("운영체제:{0}".format(os.name))
+
+import glob 
+
+result = glob.glob("c:\\work\\*.py")
+for item in result:
+    print(item)
+
+    
