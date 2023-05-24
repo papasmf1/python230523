59d70748e158ef908afb1db0ebd6df2e8e3e7ccb
papasmf1 <papasmf1@gmail.com>
2023-05-24 09:59:49 +0900

    문자열 서식 지정

---
+++ DemoFormat.py
@@ -0,0 +1,25 @@
+# DemoFormat.py 
+
+for i in range(1,11):
+    url = "http://www.ycampus.co.kr/?page=" + str(i)
+    print(url)
+
+for x in range(1,6):
+    print(x,"*",x,"=",x*x)
+
+print("---오른쪽 정렬---")
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).rjust(3))
+
+print("---0으로 채우기---")
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).zfill(5))
+
+
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:,}".format(15000))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
+
---
+++ test01.py
@@ -0,0 +1,42 @@
+import math
+
+# Basic math operations
+x = 4.3
+y = 2.7
+
+print("x =", x)
+print("y =", y)
+print("")
+
+print("Mathematical Operations:")
+print("Addition (x + y) =", x + y)
+print("Subtraction (x - y) =", x - y)
+print("Multiplication (x * y) =", x * y)
+print("Division (x / y) =", x / y)
+print("")
+
+# Trigonometric functions
+angle = math.pi / 4  # 45 degrees
+
+print("Trigonometric Functions:")
+print("Sine of", angle, "=", math.sin(angle))
+print("Cosine of", angle, "=", math.cos(angle))
+print("Tangent of", angle, "=", math.tan(angle))
+print("")
+
+# Exponential and logarithmic functions
+num = 2.5
+
+print("Exponential and Logarithmic Functions:")
+print("e^", num, "=", math.exp(num))
+print("Natural logarithm of", num, "=", math.log(num))
+print("Base-10 logarithm of", num, "=", math.log10(num))
+print("")
+
+# Power and square root
+base = 3
+exponent = 4
+
+print("Power and Square Root:")
+print(base, "raised to the power of", exponent, "=", math.pow(base, exponent))
+print("Square root of", base, "=", math.sqrt(base))
